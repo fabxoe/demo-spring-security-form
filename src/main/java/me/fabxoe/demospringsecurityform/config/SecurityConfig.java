5a662037dd2b560007b79dbd7e5aea8330f0081c
Sungmin Oh <fabxoe.kor@gmail.com>
2020-02-16 18:11:13 +0900

    스프링 시큐리티 아키텍처 설명 마무리 직후

--- src/main/java/me/fabxoe/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/me/fabxoe/demospringsecurityform/config/SecurityConfig.java
@@ -5,17 +5,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+import org.springframework.security.web.access.expression.WebExpressionVoter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
-@Order(Ordered.LOWEST_PRECEDENCE - 50)
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    AccountService accountService;
+//    public AccessDecisionManager accessDecisionManager() {
+//        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+//        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+//
+//        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+//        handler.setRoleHierarchy(roleHierarchy);
+//
+//        WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
+//        webExpressionVoter.setExpressionHandler(handler);
+//
+//        List<AccessDecisionVoter<? extends Object>> voters = Arrays.asList(webExpressionVoter);
+//
+//        return new AffirmativeBased(voters);
+//    }
+
+    public SecurityExpressionHandler expressionHandler() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+
+        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+        handler.setRoleHierarchy(roleHierarchy);
+
+        return handler;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,7 +57,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/", "/info","/account/**").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
                 .mvcMatchers("/user").hasRole("USER")
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+//                .accessDecisionManager(accessDecisionManager()
+                .expressionHandler(expressionHandler());
         http.formLogin();
         http.httpBasic();
     }
